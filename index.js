37a505afdcd2bfdb804285d50076db651eeb7850
bredele <olivier.wietrich@gmail.com>
2017-06-29 14:34:30 -0700

    Add dependencies

---
+++ index.js
@@ -0,0 +1,18 @@
+/**
+ * Dependencies.
+ */
+
+const through = require('through2')
+const EOL = require('os').EOL
+const stringify = require('json-stringify-safe')
+
+
+/**
+ *
+ */
+ 
+module.exports = () => {
+  return through.obj(opts, function(obj, enc, cb) {
+    cb(null, stringify(obj) + EOL)
+  })
+}
