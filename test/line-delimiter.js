80eb8452c39db5186955c14cab2b11a1287eb776
bredele <olivier.wietrich@gmail.com>
2017-06-29 14:41:42 -0700

    Serialize data and add line break

--- test/line-delimiter.js
+++ test/line-delimiter.js
@@ -3,6 +3,7 @@
  */
 
 const test = require('tape')
+const concat = require('concat-stream')
 const transform = require('..')
 
 test('should be a duplex stream', assert => {
@@ -10,5 +11,17 @@ test('should be a duplex stream', assert => {
   const stream = transform()
   stream.on('data', data => assert.ok('data received'))
   stream.write({})
+  stream.end()
+})
+
+
+test('should add line delimiter to chunks od strings', assert => {
+  assert.plan(1)
+  const stream = transform()
+  stream.pipe(concat(str => {
+    assert.equal(str, `"hello"\n"world"\n`)
+  }))
+  stream.write('hello')
+  stream.write('world')
   stream.end()
 })
